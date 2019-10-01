4d3a199b2d9ee1b8b5922475e6f9d7eb1b86ef64
Kuyen <iamcxa@gmail.com>
2019-10-01 16:45:26 +0800

    fix start script

--- socket.js
+++ socket.js
@@ -1,6 +1,11 @@
 var net = require("net");
 
+// The port number you want to use for socket connection
 var PORT = 3000;
+
+// The IP address you want to use for socket connection,
+// this will be the address your client is listening on.
+// You can use 127.0.0.1 or your lan ip address.
 var HOST = "127.0.0.1";
 
 var server = net.createServer(function(socket) {
